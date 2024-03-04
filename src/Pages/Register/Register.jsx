1bf5ebef21d71288f168eefcbaa588b47f9871ea
Md Shojib Hossain <mdshojib922@gmail.com>
2024-03-04 21:06:21 +0600

    Register Page Complted

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,9 +1,135 @@
 
+import { Link } from "react-router-dom";
+import { AiOutlineMail } from 'react-icons/ai';
+import { RiLockPasswordFill } from 'react-icons/ri';
+import { BsFillPersonFill, BsFillFileImageFill } from 'react-icons/bs';
+import { useState } from "react";
+import { FaEyeSlash, FaEye } from 'react-icons/fa';
+import Swal from 'sweetalert2';
+import Lottie from "lottie-react";
+import animation from '../../assets/SignUpAnimation.json'
+
 
 const Register = () => {
+
+    let [showPassword, setShowPassword] = useState(false);
+
+    // let navigate = useNavigate()
+    let handleCreateUser = (e) => {
+        e.preventDefault();
+        let myname = e.target.name.value;
+        let myphoto = e.target.image.value;
+        let email = e.target.email.value;
+        let password = e.target.password.value;
+
+        let info ={
+            myname: myname,
+            myphoto: myphoto,
+            email: email,
+            password: password
+        }
+        console.log(info)
+
+        if (password.length < 6) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Password must be more than 6 character',
+            })
+            return;
+        }
+        else if (!/[A-Z]/.test(password)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Password must at-least one Capital letter',
+            })
+            return;
+        }
+        // eslint-disable-next-line no-useless-escape
+        else if (!/.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\-].*/.test(password)) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Password must at-least one Special Charecter',
+            })
+            return;
+        }
+    }
+
     return (
-        <div>
-            <p>Hi! I am Register </p>
+        <div className="flex flex-col md:flex-row gap-5 px-2 justify-center items-center pt-5">
+            <div className="bg-gray-400 w-full md:w-5/12 text-center p-10 rounded-lg">
+                <h2 className="text-3xl font-bold mb-2">Register Now!</h2>
+                <form onSubmit={handleCreateUser}>
+                    {/* User Name */}
+                    <div className="relative">
+                        <p className="text-left text-lg font-semibold">User Name</p>
+                        <BsFillPersonFill className="absolute bottom-4 left-2"></BsFillPersonFill>
+                        <input className="w-full p-2 pl-7 text-black rounded-lg my-1"
+                            type="text"
+                            name="name"
+                            placeholder="Type your name" required
+                        />
+                    </div>
+                    <hr className="my-2" />
+                    {/* User Email */}
+                    <div className="relative">
+                        <p className="text-left text-lg font-semibold">User Email</p>
+                        <AiOutlineMail className="absolute bottom-4 left-2"></AiOutlineMail>
+                        <input className="w-full p-2 pl-7 text-black rounded-lg my-1"
+                            type="email"
+                            name="email"
+                            placeholder="Type your email"
+                            required />
+                    </div>
+                    <hr className="my-2" />
+                    {/* Password */}
+                    <div className="relative">
+                        <p className="text-left text-lg font-semibold">Password</p>
+                        <RiLockPasswordFill className="absolute bottom-4 left-2"></RiLockPasswordFill>
+                        <input className="w-full p-2 pl-6 text-black rounded-lg my-1"
+                            type={showPassword ? 'text' : 'password'}
+                            name="password"
+                            placeholder="Type Password" required
+                        />
+                        <span onClick={() => setShowPassword(!showPassword)} className="absolute right-3 bottom-4">{showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}</span>
+                    </div>
+                    <hr className="my-2" />
+                    {/* Confirm Password */}
+                    <div className="relative">
+                        <p className="text-left text-lg font-semibold">Confirm Password</p>
+                        <RiLockPasswordFill className="absolute bottom-4 left-2"></RiLockPasswordFill>
+                        <input className="w-full p-2 pl-6 text-black rounded-lg my-1"
+                            type={showPassword ? 'text' : 'password'}
+                            name="password"
+                            placeholder="Confirm Password" required
+                        />
+                        <span onClick={() => setShowPassword(!showPassword)} className="absolute right-3 bottom-4">{showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}</span>
+                    </div>
+                    <hr className="my-2" />
+                    <div className="relative">
+                        <p className="text-left text-lg font-semibold">Select Image</p>
+                        <BsFillFileImageFill className="absolute bottom-4 left-2"></BsFillFileImageFill>
+                        <input className="w-full rounded-full p-2 pl-7 text-black my-1"
+                            type="file"
+                            name="image"
+                            required
+                        />
+                    </div>
+                    <button
+                        className="bg-gradient-to-r from-violet-500 to-fuchsia-500 w-full py-2 text-white font-semibold text-lg rounded-xl" type="submit">
+                        Register</button>
+
+                </form>
+                <div className="flex gap-3 justify-center mt-3">
+                    <p>Already have an account?</p>
+                    <Link className="underline text-lg text-blue-600" to='/login'>Login now</Link>
+                </div>
+            </div>
+            <div className="">
+                <Lottie className="h-[630px] w-full md:w-10/12 " animationData={animation} loop={true} ></Lottie>
+            </div>
         </div>
     );
 };
